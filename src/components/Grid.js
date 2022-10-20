25b90517b655a3277c9722083812b7b32cb4bd3f
elenosis <eleni.orfanou.88@gmail.com>
2022-10-20 17:18:55 +0200

    grid

---
+++ src/components/Grid.js
@@ -0,0 +1,52 @@
+import "./grid.css";
+
+const Grid = () => {
+  return (
+    <div class="grid">
+      <div class="box">10:00</div>
+      <div class="box"></div>
+      <div class="box">⏰</div>
+      <div class="box">Tuesday</div>
+      <div class="box">Vacation</div>
+      <div class="box">October</div>
+      <div class="box">7</div>
+      <div class="box">December</div>
+      <div class="box">9</div>
+      <div class="box"></div>
+      <div class="box">Christmas</div>
+      <div class="box">12</div>
+      <div class="box">13</div>
+      <div class="box">14</div>
+      <div class="box">15</div>
+      <div class="box">16</div>
+      <div class="box">17</div>
+      <div class="box">18</div>
+      <div class="box">19</div>
+      <div class="box">20</div>
+      <div class="box">21</div>
+      <div class="box">22</div>
+      <div class="box">23</div>
+      <div class="box">24</div>
+      <div class="box">25</div>
+      <div class="box">26</div>
+      <div class="box">:)</div>
+      <div class="box">28</div>
+      <div class="box">29</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">:(</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+      <div class="box">30</div>
+    </div>
+  );
+};
+
+export default Grid;
