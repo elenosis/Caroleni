3e9c6057d2b3b453e36bef8cbe62ab21b886b0a9
elenosis <eleni.orfanou.88@gmail.com>
2022-10-20 11:44:44 +0200

    wetter

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
+const MY_KEY = "866470bfa7b122c147a6ef7230d9f2b3";
+
 const Weather = () => {
-  return <div>Weather</div>;
+  const [weather, setWeather] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${MY_KEY}&units=metric&lang=en`
+    )
+      .then((response) => response.json())
+      .then((data) => setWeather(data));
+  });
+  return <div>{weather}</div>;
 };
 
 export default Weather;
