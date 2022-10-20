a0260830389bb24a2b7c33ef85d3c9c6a1b58df7
Carola Zapp <carola.zapp@gmx.net>
2022-10-20 19:28:02 +0200

    user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,43 @@
-const User = () => {
-  return <div>User</div>;
+const user = {
+  id: "1",
+  firstname: "Carola",
+  lastname: "Zapp",
+  password: "huhu4",
+  birthday: "04.05.1968",
+  city: "Lindlar",
+  street: "Zum Bayenhof 5",
+  phone: "+49 1234 56789",
+  mobile: "+49 1234 56789",
+  email: "zappcarola@gmail.com",
+  events: [
+    {
+      date: "",
+      title: "",
+      time: "",
+      description: "",
+    },
+  ],
+  diary: [
+    {
+      date: "",
+      feel: "",
+      discription: "",
+    },
+  ],
+  contacts: [
+    {
+      firstname: "Hugo",
+      lastname: "Alos",
+      password: "huhu5",
+      birthday: "15.10.2000",
+      city: "Berlin",
+      street: "Niemetzstrassse 27",
+      phone: "+49 1234 56788",
+      mobile: "+49 1234 56788",
+      email: "aloshugo@gmail.com",
+      images: [
+        "https://upload.wikimedia.org/wikipedia/commons/e/e1/British_blue_2009_%28cropped%29.jpg",
+      ],
+    },
+  ],
 };
-
-export default User;
