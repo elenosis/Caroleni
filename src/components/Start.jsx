6e6c193c35d7fcce547fefb643e71873d3d2bb6d
Carola Zapp <carola.zapp@gmx.net>
2022-10-20 12:35:12 +0200

    Sicherung

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
 import "./Start.css";
+import background from "../images/Start.jpg";
 
 const Start = () => {
   return (
     <div className="containerStart">
-      <div></div>
+      <div style={{ backgroundImage: `url(${background})` }}></div>
       <div>Carpe Diem with Caroleni</div>
       <NavLink to="login" className={"navLink"}>
         Login
