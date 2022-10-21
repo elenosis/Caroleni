a9ed1e6273a8a5ccea946f4641cd9ead8d7d9032
elenosis <eleni.orfanou.88@gmail.com>
2022-10-21 08:48:03 +0200

    grid-start

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,48 +3,52 @@ import "./grid.css";
 const Grid = () => {
   return (
     <div class="grid">
+      <div class="box">🤸🏼‍♀️</div>
       <div class="box">10:00</div>
-      <div class="box"></div>
       <div class="box">⏰</div>
       <div class="box">Tuesday</div>
-      <div class="box">Vacation</div>
-      <div class="box">October</div>
-      <div class="box">7</div>
+      <div class="box">🧘</div>
+
+      <div class="box">30</div>
       <div class="box">December</div>
       <div class="box">9</div>
       <div class="box"></div>
       <div class="box">Christmas</div>
       <div class="box">12</div>
-      <div class="box">13</div>
-      <div class="box">14</div>
-      <div class="box">15</div>
-      <div class="box">16</div>
-      <div class="box">17</div>
-      <div class="box">18</div>
-      <div class="box">19</div>
+      <div class="box">✈️</div>
+      <div class="box">September</div>
+      <div class="box">🍕</div>
       <div class="box">20</div>
-      <div class="box">21</div>
-      <div class="box">22</div>
-      <div class="box">23</div>
+
+      <div class="box">👩🏻‍⚕️</div>
+      <div class="box">February</div>
       <div class="box">24</div>
-      <div class="box">25</div>
-      <div class="box">26</div>
+      <div class="box">5</div>
+      <div class="box">12:30</div>
       <div class="box">:)</div>
-      <div class="box">28</div>
+      <div class="box">🪘</div>
       <div class="box">29</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
+      <div class="box">🥪</div>
+      <div class="box">Monday</div>
+      <div class="box">🏝</div>
+      <div class="box">October</div>
+      <div class="box"></div>
+      <div class="box">31</div>
       <div class="box">30</div>
       <div class="box">:(</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
-      <div class="box">30</div>
+      <div class="box">🎂</div>
+      <div class="box">August</div>
+      <div class="box">12</div>
+      <div class="box"></div>
+      <div class="box">January</div>
+      <div class="box">17:00</div>
+      <div class="box">29</div>
+      <div class="box">🧑‍💻</div>
+      <div class="box">Monday</div>
+      <div className="enter_caroleni">
+        <h3>Organise your life with Caroleni</h3>
+        <button>Click to enter</button>
+      </div>
     </div>
   );
 };
